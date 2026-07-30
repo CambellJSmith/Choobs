d9ee1ddb67e274018e27dfbcde23a5e76ce49c7e
Animatic_Almanac <67105502+CambellJSmith@users.noreply.github.com>
2026-07-30 17:51:36 +0100

    Migrate legacy Other campaign autosaves

--- js/campaign_migrations.js
+++ js/campaign_migrations.js
@@ -3,6 +3,7 @@
 
     const PROGRESS_KEY = "choobs_campaign_progress_v1";
     const LEGACY_KEY = "choobs_completed_levels";
+    const AUTOSAVE_KEY = "choobs_autosave_v1";
     const OTHER_LEVELS = Object.freeze({
         18: 1,
         19: 2,
@@ -27,6 +28,16 @@
             .filter(([legacy_number]) => legacy.has(Number(legacy_number)))
             .map(([, campaign_number]) => campaign_number);
         localStorage.setItem(PROGRESS_KEY, JSON.stringify(progress));
+
+        const autosave = JSON.parse(localStorage.getItem(AUTOSAVE_KEY) || "null");
+        const legacy_number = Number(autosave && autosave.level_number);
+
+        if (autosave && !autosave.campaign_id &&
+            Object.prototype.hasOwnProperty.call(OTHER_LEVELS, legacy_number)) {
+            autosave.campaign_id = "Other";
+            autosave.level_number = OTHER_LEVELS[legacy_number];
+            localStorage.setItem(AUTOSAVE_KEY, JSON.stringify(autosave));
+        }
     } catch (error) {
         console.warn("Other campaign progress could not be migrated.", error);
     }
